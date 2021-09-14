0a2fc3d7590e7f205fd34972ed048b9b7a07640b
Narendra404 <npaul7345@gmail.com>
2021-09-14 10:13:39 +0530

    Modify the main c file to properly implement clear screen function

--- calculator.c
+++ calculator.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
-
-#ifdef _WIN32
-#include <conio.h>
-#else
-#include <stdio.h>
-#define clrscr() printf("\e[1;1H\e[2J")
-#endif
+#define clear() printf("\e[1;1H\e[2J")
 
 int main(void)
 {
     for (int i = 0; i < 10; i++)
     {
         printf("%d\n", i + 1);
-        clrscr();
+        clear();
     }
 
     printf("Completed.\n");
