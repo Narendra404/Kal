db48bffe91fdb92c8371f9f984eee0200fcb89cd
Narendra404 <npaul7345@gmail.com>
2021-09-15 14:51:29 +0000

    Modify calculator.c to make a rectangle

--- calculator.c
+++ calculator.c
@@ -5,11 +5,19 @@ int main(void)
 {
     for (int i = 0; i < 10; i++)
     {
-        printf("%d\n", i + 1);
-        clear();
+        printf("* ");
+        for (int j = 0; j < 18; j++)
+        {
+            if((i == 0) || (i == 9))
+            {
+                printf("* ");
+            }
+            else 
+            {
+                printf("  ");
+            }
+        }
+        printf("*\n");
     }
-
-    printf("Completed.\n");
-
     return 0;
 }
