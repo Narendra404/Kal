8bb47c357feb25a975c4331ecdcd62b56f0e41f8
Narendra404 <npaul7345@gmail.com>
2021-09-18 17:01:53 +0000

    Created a Screen function and the input stack

--- calculator.c
+++ calculator.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #define clear() printf("\e[1;1H\e[2J")
-#define N 20
+#define N 80
 
-void func(void);
+void Screen();
+void keys(int k);
+
+char InputStack[8];
 
 int main(void)
 {
@@ -11,22 +14,76 @@ int main(void)
     return 0;
 }
 
-void func(void)
+void Screen()
 {
-    for (int i = 0; i < N; i++)
-    {
-        printf("* ");
-        for (int j = 0; j < ((2 * N) - 2); j++)
+    InputStack[0] = '2';
+    InputStack[1] = '3';
+
+    for (int k = 0; k < 2; k++)
+    {   
+        for (int i = 0; i < N; i++)
         {
-            if((i == 0) || (i == 4) || (i == (N - 1)))
-            {
-                printf("* ");
-            }
-            else 
-            {
-                printf("  ");
-            }
+            printf("*");
         }
-        printf("*\n");
+        printf("\n");
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("** ");
+        keys(i);
+        printf("**\n");
     }
 }
+
+void keys(int k)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        switch((int)InputStack[i])
+        {
+            case 50:
+            switch(k)
+            {
+                case 0:
+                printf("***** ");
+                break;
+                case 1:
+                printf("    $ ");
+                break;
+                case 2:
+                printf("$***$ ");
+                break;
+                case 3:
+                printf("$     ");
+                break;
+                case 4:
+                printf("***** ");
+                break;
+            }
+            break;
+
+            case 51:
+            switch(k)
+            {
+                case 0:
+                printf("***** ");
+                break;
+                case 1:
+                printf("    $ ");
+                break;
+                case 2:
+                printf("$***$ ");
+                break;
+                case 3:
+                printf("    $ ");
+                break;
+                case 4:
+                printf("***** ");
+                break;
+            }
+            break;
+        }
+    }
+}
+
