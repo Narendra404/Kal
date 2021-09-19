0321e5caf1dafd105eef3f29f01900df4f6a80b6
Narendra404 <npaul7345@gmail.com>
2021-09-19 05:17:45 +0000

    Add PopChar, Line and ValidChar

--- calculator.c
+++ calculator.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #define clear() printf("\e[1;1H\e[2J")
 #define N 80
 
 void Screen();
 void input();
 void keys(int k);
+void ClearStack();
+int PushChar(char ch);
+void PopChar();
+void Line();
+int ValidChar(char ch);
 
-char InputStack[8];
+char InputStack[8] = {'n'};
 
 int main(void)
 {
-        func();
+    Screen();
 
     return 0;
 }
 
 void Screen()
 {
-    InputStack[0] = '2';
-    InputStack[1] = '3';
-
-    for (int k = 0; k < 2; k++)
-    {   
-        for (int i = 0; i < N; i++)
-        {
-            printf("*");
-        }
-        printf("\n");
-    }
+    Line();
+    Line();
 
     for (int i = 0; i < 5; i++)
     {
@@ -36,6 +34,22 @@ void Screen()
         printf("**\n");
     }
 }
+
+void Line()
+{        
+    for (int i = 0; i < N; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+}
+
+
+void Keypad()
+{
+    Line();
+    Line();
 
 void keys(int k)
 {
@@ -88,14 +102,37 @@ void keys(int k)
     }
 }
 
+void ClearStack()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        InputStack[i] == 'n';
+    }
+}
+
+int PushChar(char ch)
+{
+    if (InputStack[0] == 'n')
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            InputStack[i - 1] = InputStack[i];
+        }
+
+        InputStack[9] = ch;
+
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 void input()
 {
     if (AnsFlag == 1)
     {
-        for (int i = 0; i < 10; i++)
-        {
-            InputStack[i] == 'n';
-        }
+        ClearStack();    
     }
 
     do
@@ -110,3 +147,122 @@ void input()
         }
     }
     while(ValidChar(ch));
+
+    if (isdigit(ch))
+    {
+        if (!PushChar(ch))
+        {
+            printf("Maximum capacity to hold a number has been reached.\n");
+            printf("Please enter a smaller number.\n");
+
+            ClearStack();
+        }
+    }
+    else if (isalpha(ch))
+    {
+        if (ch == 'd')
+        {
+            PopChar();
+        }
+
+        if (ch == 'q')
+        {
+            exit(0);
+        }
+    }
+    else
+    {
+        ClearStack();
+        PushChar(ch);
+    }
+}
+
+void PopChar()
+{
+    for (int i = 0; i < 9; i++)
+    {
+        InputStack[i + 1] = InputStack[i];
+    }
+
+    InputStack[0] = 'n';
+}
+
+int ValidChar(char ch)
+{
+    switch(ch)
+    {
+        case '1':
+        return 0;
+        break;
+
+        case '2':
+        return 0;
+        break;
+
+        case '3':
+        return 0;
+        break;
+
+        case '4':
+        return 0;
+        break;
+
+        case '5':
+        return 0;
+        break;
+
+        case '6':
+        return 0;
+        break;
+
+        case '7':
+        return 0;
+        break;
+
+        case '8':
+        return 0;
+        break;
+
+        case '9':
+        return 0;
+        break;
+
+        case '0':
+        return 0;
+        break;
+
+        case '+':
+        return 0;
+        break;
+
+        case '-':
+        return 0;
+        break;
+
+        case '*':
+        return 0;
+        break;
+
+        case '/':
+        return 0;
+        break;
+
+        case '=':
+        return 0;
+        break;
+
+        case 'd':
+        return 0;
+        break;
+
+        case 'q':
+        return 0;
+        break;
+
+        default :
+        return 1;
+        break;
+    }
+}
+
+
